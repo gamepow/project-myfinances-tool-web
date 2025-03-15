aaf5f72eb7d498bcfb729cc288238842a97d33be
Daniel carvajal <carvajal629@gmail.com>
2025-03-15 15:51:28 -0600

    Login Page

---
+++ src/Login.js
@@ -0,0 +1,70 @@
+import logo from './logo.svg';
+import './Login.css';
+import React, { useState } from 'react';
+
+function Login() {
+  const [username, setUserName] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch('http://localhost:8080/api/auth/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ username, password }),
+      });
+
+      if (response.ok) {
+        
+        const data = await response.json();
+        // Store the JWT token in local storage or cookies
+        localStorage.setItem('token', data.token);
+        alert("Login Exitoso!");
+        // TODO: add redirect to home page
+        // window.location.href = '/main';
+
+      } else {
+        const errorMessage = await response.text();
+        setError(errorMessage);
+      }
+    } catch (error) {
+      setError("An error occurred during login: " + error);
+    }
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      {error && <p style={{ color: 'red' }}>{error}</p>}
+      <form onSubmit={handleLogin}>
+        <label>
+          Username:
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => setUserName(e.target.value)}
+          />
+        </label>
+        <br />
+        <label>
+          Password:
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+        <br />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+
+}
+
+export default Login;
